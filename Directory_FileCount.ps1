7535851cfa09ab5c87198485c9d3c30ffe22da48
Tatsuya-kanaria <kanaria.git@gmail.com>
2024-04-14 14:44:31 +0900

    add Directory_FileCount

---
+++ Directory_FileCount.ps1
@@ -0,0 +1,6 @@
+# targetDir のディレクトリ毎のファイル数を数える
+$targetDir = ‘./test'
+# ディレクトリの一覧取得
+$dirs = Get-Childitem -Path $targetDir -Recurse -Directory
+# ％：foreach
+$dirs | %{ write-host 場所:$_.FullName, ファイル合計:(Get-ChildItem $_ -recurse -File | Measure-Object).count ,  内数(Get-ChildItem $_ -File | Measure-Object).count}
